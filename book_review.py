ed0a1ecc38213ea1ab5d7e14cff1d32cee1ce501
omar-xy <otaraki@student.1337.ma>
2024-01-30 00:44:14 +0100

    done

--- book_review.py
+++ book_review.py
@@ -14,17 +14,14 @@ class BookReview:
         
         
     def get_book_rating(self, sort="ASC", max_records=10):
-        rating  = ["rating"]
-        # for rating in rating:
-        if sort == "DESC":
+        if not sort:
+            return self.table.all(max_records=max_records)
+        elif sort == "ASC":
+            rating = ["rating"]
+        elif sort == "DESC":
             rating = ["-rating"]
-        table = self.table.all(sort=rating, max_records=max_records)
-        # records = self.table.all()
-        
-        # for record in records:
-        #     if record['fields']['Book'] == book_name:
-        #         return record['fields']['rating']
-        return table
+        record = self.table.all(sort=rating, max_records=max_records)
+        return record
     
     def add_book_rating(self, book_name, rating, notes=None):
         records = self.table.all()
@@ -39,8 +36,8 @@ class BookReview:
 
 
 
-if __name__ == '__main__':
-    br = BookReview()
-    i = br.get_book_rating(sort="ASC", max_records=1)
-    print(i)  
+# if __name__ == '__main__':
+#     br = BookReview()
+#     i = br.get_book_rating(sort="ASC", max_records=1)
+#     print(i)  
     # print(br.add_book_rating('AOT', 5, 'This is a great book'))
